187a97ac1c6784f6255fe6a6441d5745b7af8405
Charles Parsley <parsley128@gmail.com>
2019-04-04 17:32:34 -0500

    hi

---
+++ sandboxScript.js
@@ -0,0 +1,7 @@
+acceptableInputs = ["something","somethimg else"]
+
+function acceptableInputs() {
+    for (let i = 0; i < acceptableInputs.length; i++) {
+        console.log(acceptableInputs);
+    }
+}
